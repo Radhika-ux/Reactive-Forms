652ff8508b59b93fb35027c3bff4fd5916ab3937
Radhika-ux <boilaradhika1@gmail.com>
2022-05-29 22:05:54 +0530

    Reactive form is created as per the given requirement

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveformsComponent } from './reactiveforms/reactiveforms/reactiveforms.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactiveformsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
